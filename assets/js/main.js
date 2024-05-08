9e9513143a237489da6a1822b91c2bc1e8374728
berdibek-2002 <uralov0212@gmail.com>
2024-05-08 11:00:23 +0500

    version 1.0.1

---
+++ assets/js/main.js
@@ -0,0 +1,90 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+  const navMenu = document.getElementById('nav-menu');
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show-menu');
+};
+navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+/*==================== VIDEO ====================*/
+const videoFile = document.getElementById('video-file'),
+  videoButton = document.getElementById('video-button'),
+  videoIcon = document.getElementById('video-icon');
+
+function playPause() {
+  if (videoFile.paused) {
+    // Play video
+    videoFile.play();
+    // We change the icon
+    videoIcon.classList.add('ri-pause-line');
+    videoIcon.classList.remove('ri-play-line');
+  } else {
+    // Pause video
+    videoFile.pause();
+    // We change the icon
+    videoIcon.classList.remove('ri-pause-line');
+    videoIcon.classList.add('ri-play-line');
+  }
+}
+videoButton.addEventListener('click', playPause);
+
+function finalVideo() {
+  // Video ends, icon change
+  videoIcon.classList.remove('ri-pause-line');
+  videoIcon.classList.add('ri-play-line');
+}
+// ended, when the video ends
+videoFile.addEventListener('ended', finalVideo);
+
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? 'dark' : 'light');
+const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun');
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+});
